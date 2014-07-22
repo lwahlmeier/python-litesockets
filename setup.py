0d50a438dca61d225de048dc581ba92296bb7bb7
Luke Wahlmeier <lwahlmeier@gmail.com>
2014-07-22 09:10:18 -0600

    moved to setuptools

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 
 setup (# Distribution meta-data
        name = "litesockets",
-       version = "0.2.5",
+       version = "0.2.6",
        author = "Luke Wahlmeier",
        author_email = "lwahlmeier@gmail.com",
        url = "https://github.com/lwahlmeier/python-litesockets",
-       download_url = "https://github.com/lwahlmeier/python-litesockets/tarball/0.2.5",
+       download_url = "https://github.com/lwahlmeier/python-litesockets/tarball/0.2.6",
        license = "lgpl",
        description = "A multi threaded socket library for python",
-       requires = ['threadly'],
+       install_requires = ['threadly'],
        packages =  ['litesockets'],
        platforms = ('linux',),
        keywords = ("networking",),
+       test_suite="tests",
       )
