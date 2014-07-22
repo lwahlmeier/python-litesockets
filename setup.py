4b4c8cddcbecf4a5861c4a3d58bf6e999168f4a1
Luke Wahlmeier <lwahlmeier@gmail.com>
2014-07-22 09:30:54 -0600

    more updates for pypi

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ from setuptools import setup
 
 setup (# Distribution meta-data
        name = "litesockets",
-       version = "0.2.6",
+       version = "0.2.7",
        author = "Luke Wahlmeier",
        author_email = "lwahlmeier@gmail.com",
        url = "https://github.com/lwahlmeier/python-litesockets",
-       download_url = "https://github.com/lwahlmeier/python-litesockets/tarball/0.2.6",
+       download_url = "https://github.com/lwahlmeier/python-litesockets/tarball/0.2.7",
+       test_suite = "tests",
        license = "lgpl",
        description = "A multi threaded socket library for python",
        install_requires = ['threadly'],
        packages =  ['litesockets'],
-       platforms = ('linux',),
-       keywords = ("networking",),
-       test_suite="tests",
+       keywords = ["networking"],
       )
